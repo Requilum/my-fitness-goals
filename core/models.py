74067cb0fe91267854be2321f5d7ef54545f6523
aalma <aalma@example.com>
2025-04-29 20:07:55 +0300

    feat: create models and migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Goals(models.Model):
+    goal = models.CharField(max_length=255)
+    description = models.TextField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=50, choices=[('Not Started', 'Not Started'), ('In Progress', 'In Progress'), ('Completed', 'Completed')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.goal
+    
+class Workouts(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.IntegerField()  
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
