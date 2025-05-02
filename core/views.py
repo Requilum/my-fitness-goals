47d05d4a660ef300addef6888fedc6fb3b39e419
aalma <aalma@example.com>
2025-05-02 16:32:02 +0300

    feat: create views for Goals CRUD

--- core/views.py
+++ core/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from .models import Goal
+from .forms import GoalForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
-def home(request):
-    return render(request, 'home.html')
+@login_required
+def goals_list(request):
+    goals = Goal.objects.filter(user=request.user)
+    return render(request, 'goals/list.html', {'goals': goals})
 
-def about(request):
-    return render(request, 'about.html')
+@login_required
+def add_goal(request):
+    if request.method == 'POST':
+        form = GoalForm(request.POST)
+        if form.is_valid():
+            goal = form.save(commit=False)
+            goal.user = request.user
+            goal.save()
+            return redirect('goals')
+    else:
+        form = GoalForm()
+    return render(request, 'goals/form.html', {'form': form, 'title': 'Add Goal'})
+
+@login_required
+def edit_goal(request, pk):
+    goal = get_object_or_404(Goal, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = GoalForm(request.POST, instance=goal)
+        if form.is_valid():
+            form.save()
+            return redirect('goals')
+    else:
+        form = GoalForm(instance=goal)
+    return render(request, 'goals/form.html', {'form': form, 'title': 'Edit Goal'})
+
+@login_required
+def delete_goal(request, pk):
+    goal = get_object_or_404(Goal, pk=pk, user=request.user)
+    if request.method == 'POST':
+        goal.delete()
+        return redirect('goals')
+    return render(request, 'goals/list.html', {'goals': Goal.objects.filter(user=request.user)})
